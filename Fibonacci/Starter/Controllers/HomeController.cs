f81ab9cb3956a39be76d018ebba9b0f8ac996fc9
Filatov Sergey <filatov@action-media.ru>
2018-06-12 16:21:24 +0700

    LP-1 Рыба приложений

---
+++ Fibonacci/Starter/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using First.Models;
+
+namespace Starter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new StarterModel {IsCalcStarted = false};
+            return View(model);
+        }
+
+        /// <summary>
+        /// Запустить расчёт
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Start(StarterModel model)
+        {
+            //Запуск потоков расчёта
+            //TODO: запуск параллельных потоков отправляющих числа на API второго приложения
+
+            model.IsCalcStarted = true;
+            return View("Index", model);
+        }
+
+        /// <summary>
+        /// Остановить расчёт
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Stop(StarterModel model)
+        {
+            //Остановка потоков расчёта
+            //TODO: остановка всех параллельных потоков вычисления
+
+            //TODO: сформировать итог вычисления всех потоков и передать через модель
+
+            model.IsCalcStarted = false;
+            return View("Index", model);
+        }
+    }
+}
---
+++ Fibonacci/Сontinuer/Models/СontinuerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Сontinuer.Models
+{
+    /// <summary>
+    /// Модель повторителя
+    /// </summary>
+    public class СontinuerModel
+    {
+        /// <summary>
+        /// Признак запуска расчётов.
+        /// Изменяется только через API при получении команд о расчётах из Starter'a
+        /// </summary>
+        public bool IsCalcStarted { get; set; }
+    }
+}
