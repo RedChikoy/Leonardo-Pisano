f0fffb17984f49e55949f2acfc301ba81465dd86
Filatov Sergey <filatov@action-media.ru>
2018-06-25 13:36:56 +0700

    LP-4 Скелет для BigNumber

---
+++ Fibonacci/BLL/Dto/BigNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Dto
+{
+    /// <summary>
+    /// Для сложения больших положительных чисел
+    /// Задел на будущее
+    /// </summary>
+    public class BigNumber
+    {
+        private byte[] _digits;
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BigNumber operator +(BigNumber left, BigNumber right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator string(BigNumber left)
+        {
+            throw new NotImplementedException();
+        }
+}
+}
